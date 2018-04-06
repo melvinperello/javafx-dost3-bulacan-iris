ffdd1fcd22c882b264bb22e9ef02f363fd24e3ee
jhmvin <izumiiii.10@gmail.com>
2018-04-06 14:45:13 +0800

    sorting spread sheet

--- src/gov/dost/bulacan/iris/ui/training/TrainingEncode.java
+++ src/gov/dost/bulacan/iris/ui/training/TrainingEncode.java
@@ -40,6 +40,7 @@ import javafx.scene.layout.VBox;
 import org.controlsfx.control.spreadsheet.GridBase;
 import org.controlsfx.control.spreadsheet.SpreadsheetCell;
 import org.controlsfx.control.spreadsheet.SpreadsheetCellType;
+import org.controlsfx.control.spreadsheet.SpreadsheetColumn;
 import org.controlsfx.control.spreadsheet.SpreadsheetView;
 
 /**
@@ -47,47 +48,107 @@ import org.controlsfx.control.spreadsheet.SpreadsheetView;
  * @author s500
  */
 public class TrainingEncode extends IrisForm {
-    
+
     @FXML
     private HBox hbox_header;
-    
+
     @FXML
     private JFXButton btn_back;
-    
+
     @FXML
     private Label lbl_modify_header;
-    
+
     @FXML
     private Label lbl_modify_time;
-    
+
     @FXML
     private JFXButton btn_save;
-    
+
     @FXML
     private VBox vbox_ss_container;
-    
+
+    public TrainingEncode() {
+        //
+    }
+    private final static String A_1 = "1. The choice of technologies topics presented";
+    private final static String A_2 = "2. Potential application of the technologies/topics presented to my work / business";
+    private final static String A_3 = "3. The additional knowledge gained from the technologies presented";
+    private final static String A_4 = "4. Potential application of the technologies presented to start a new business / upgrade existing business.";
+    private final static String A_5 = "5. Adequacy of time allotted for the presentation of the technology / topics including the open forum session.";
+    //
+    private final static String B_1 = "1. Knowledge / Competence / Grasp of the subject matter";
+    private final static String B_2 = "2. Audience Rapport";
+    private final static String B_3 = "3. Presentation Skill";
+    private final static String B_4 = "4. Time Management";
+    //
+    private final static String C_1 = "1. Conductiveness of Training Materials and Kits Provided";
+    private final static String C_2 = "2. Adequacy of Training Materials and Kits Provided";
+    private final static String C_3 = "3. Provision of Assistance to Participants";
+
     @Override
     protected void setup() {
         // row count
         int rowCount = 15;
         // column count
         int columnCount = 2;
-        // grid
+        // Create Grid Base.
         GridBase grid = new GridBase(rowCount, columnCount);
+
         // row list
         ObservableList<ObservableList<SpreadsheetCell>> rows = FXCollections.observableArrayList();
         // iterate row
         for (int row = 0; row < grid.getRowCount(); ++row) {
             // column list
             final ObservableList<SpreadsheetCell> list = FXCollections.observableArrayList();
-            // iterate column
+
             for (int column = 0; column < grid.getColumnCount(); ++column) {
                 // add column
-                SpreadsheetCell cell = SpreadsheetCellType.STRING.createCell(row, column, 1, 1, "value");
-                cell.setStyle("-fx-pref-size: 1000px");
+                String text = "";
+                SpreadsheetCell cell = SpreadsheetCellType.STRING.createCell(row, column, 1, 1, text);
+
+                if (column == colTitle && row == 0) {
+                    text = A_1;
+                } else if (column == colTitle && row == 1) {
+                    text = A_2;
+                } else if (column == colTitle && row == 2) {
+                    text = A_3;
+                } else if (column == colTitle && row == 3) {
+                    text = A_4;
+                } else if (column == colTitle && row == 4) {
+                    text = A_5;
+                } else if (column == colTitle && row == 5) {
+                    text = "TECHNOLOGY INFORMATION PRESENTED";
+                } else if (column == colTitle && row == 6) {
+                    text = B_1;
+                } else if (column == colTitle && row == 7) {
+                    text = B_2;
+                } else if (column == colTitle && row == 8) {
+                    text = B_3;
+                } else if (column == colTitle && row == 9) {
+                    text = B_4;
+                } else if (column == colTitle && row == 10) {
+                    text = "RESOURCE SPEKAER";
+                } else if (column == colTitle && row == 11) {
+                    text = C_1;
+                } else if (column == colTitle && row == 12) {
+                    text = C_2;
+                } else if (column == colTitle && row == 13) {
+                    text = C_3;
+                } else if (column == colTitle && row == 14) {
+                    text = C_3;
+                } else if (column == colTitle && row == 15) {
+                    text = "OVERALL RATING OF THE ACTIVITY";
+                }
+                cell.setItem(new String(text));
+                if (column == 0) {
+
+                    cell.setEditable(false);
+                    cell.setWrapText(true);
+
+                }
                 list.add(cell);
             }
-            
+
             rows.add(list);
         }
         // add row
@@ -95,8 +156,31 @@ public class TrainingEncode extends IrisForm {
 
         // attach grid base to spreadsheet
         SpreadsheetView spv = new SpreadsheetView(grid);
-        
+        SpreadsheetColumn columnTitle = spv.getColumns().get(0);
+        columnTitle.setFixed(true);
+        columnTitle.setMinWidth(700.0);
+        columnTitle.setPrefWidth(700.0);
+        columnTitle.setMaxWidth(700.0);
+//        columnTitle.setResizable(false);
+        SpreadsheetColumn columnValue = spv.getColumns().get(1);
+        columnValue.setFixed(true);
+        columnValue.setMinWidth(200.0);
+        columnValue.setPrefWidth(200.0);
+        columnValue.setMaxWidth(200.0);
+
         VBox.setVgrow(spv, Priority.ALWAYS);
         this.vbox_ss_container.getChildren().add(spv);
     }
+
+    /**
+     * Creates the framework of the spreadsheet. this segment focuses on the
+     * creation and value of the cell only, no editing of width or any other
+     * appearance related options.
+     *
+     * @return
+     */
+    private GridBase createGridFramework() {
+
+    }
+
 }
